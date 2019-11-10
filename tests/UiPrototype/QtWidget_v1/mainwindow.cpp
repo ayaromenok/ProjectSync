0da6d36e310bbbdd54f58bc901f13ae5aafd02fa
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-11-10 13:46:44 +0100

    #1 filled test UI with some stuff

--- tests/UiPrototype/QtWidget_v1/mainwindow.cpp
+++ tests/UiPrototype/QtWidget_v1/mainwindow.cpp
@@ -21,5 +21,39 @@ MainWindow::~MainWindow()
 void
 MainWindow::initTestUi()
 {
+    QStringList slBranches;
+    QStringList slProjectHeaders;
+    QList<QTreeWidgetItem *> prjItems;
+    QStringList slFileList;
 
+    //prj
+    slProjectHeaders << "PrjName" << "LastModified" << "NoIdea";
+    for (int i = 0; i < 5; ++i){
+        QTreeWidgetItem *item = new QTreeWidgetItem((QTreeWidget*)0, QStringList(QString("prj_%1").arg(i)));
+        item->addChild(new QTreeWidgetItem((QTreeWidget*)0, QStringList(QDate::currentDate().toString())));
+        prjItems.append(item);
+    }
+    ui->twPrjTree->setHeaderLabels(slProjectHeaders);
+    ui->twPrjTree->insertTopLevelItems(0,prjItems);
+
+    slBranches << "00_branch" << "01_branch" << "02_branchXXX" << "03_branchYYY";
+    ui->lwPrjBranches->insertItems(0,slBranches);
+
+    //files
+    slFileList << "file000.txt" << "fileAbcDef.txt" << "someStupidName.doc";
+    ui->lwFileList->insertItems(0,slFileList);
+    ui->teDiffLeft->append("some text at\n\t LEFT\nside");
+    ui->teDiffRight->append("some text at\n\t RIGHT\nside");
+    ui->lwGitLog->addItem("git log #0");
+    ui->lwGitLog->addItem("git log #1");
+    ui->lwGitLog->addItem("git log #2");
+
+    //issues
+    ui->lwIssueList->addItem("issue #1");
+    ui->lwIssueList->addItem("issue #2");
+    ui->lwIssueList->addItem("issue #3");
+
+    ui->lwIssueBody->addItem("body of\nIssue #1");
+    ui->lwIssueComments->addItem("git commit #1 related to \nissue #1");
+    ui->lwIssueComments->addItem("some comment to issue #1");
 }

7a729593d06c7161ef78ef08dac11bd0acaad9f0
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-11-26 18:44:00 +0100

    #4 added task for tomorrow

--- src/main/yissue.h
+++ src/main/yissue.h
@@ -24,11 +24,19 @@ private:
     QString         _createdAt; //need to convert to quint64
     QString         _updatedAt; //need to convert to quint64
     QString         _closedAt; //need to convert to quint64
-    QString         _user;      //need own holder
-    QString         _milestone; //need own holder
+    QString         _user;      //need own holder - YUser
+    QString         _milestone; //need own holder - YMilesone
     int             _timeEst;
     int             _timeSpent;
     int             _weight;
+    //assignees - array of obj - YUser
+    //author - obj - YUser
+    //assignee - obj - YUser
+    //up/down votes - int
+    //web_url - str
+    //links - obj
+    //user_notes_count - int and link from above
+    // notes required own holder - YIssueNotes
 };
 
 #endif // YISSUE_H

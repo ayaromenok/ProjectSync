7a507a21cb6da0806f7a618da1d74bf5424737c6
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-11-26 17:57:35 +0100

    #4 added more helper for YIssue

--- ypsConsole/main.cpp
+++ ypsConsole/main.cpp
@@ -3,12 +3,26 @@
 
 #include "../src/main/ygitlab.h"
 //#include "../src/main/yrequest.h"
+#include "../src/main/yissue.h"
 
 void fnHelper()
 {
     QFile file(":/test/issues.txt");
     if (file.exists()){
-            qDebug() << "file: " << file.size() << "bytes";
+        qDebug() << "file: " << file.size() << "bytes";
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)){
+            QByteArray ba = file.readAll();
+            QJsonDocument jDoc = QJsonDocument::fromJson(ba);
+            if (jDoc.isArray()) {
+                QJsonArray ja = jDoc.array();
+                for (int i=0; i< ja.size(); ++i){
+                    YIssue issue;
+                    QJsonObject jo = ja[i].toObject();
+                    //issue.parseIssue(jo);
+                    //issue.dumpToConsole();
+                }
+            }
+        }
     }
 }
 

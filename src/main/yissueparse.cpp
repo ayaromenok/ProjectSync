188ff7228107408f63b8ede7ade30dfe31a9913a
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-11-27 10:17:45 +0100

    #7 parse updated issue

--- src/main/yissueparse.cpp
+++ src/main/yissueparse.cpp
@@ -26,6 +26,22 @@ YIssueParse::parseIssue(const QJsonArray &ja, QList<YIssue*> &il)
         if (jo.contains("iid"))        { issue->setIid(jo["iid"].toInt()); }
         if (jo.contains("project_id")) { issue->setProjectId(jo["project_id"].toInt()); }
         if (jo.contains("title"))      { issue->setTitle(jo["title"].toString()); }
+        if (jo.contains("description")){ issue->setDescr(jo["description"].toString()); }
+        if (jo.contains("state"))      { issue->setState(jo["state"].toString().contains("open")?1:0); }
+//        if (jo.contains("created_at")) { issue->setCreatedAt(jo["created_at"].toString()); }
+//        if (jo.contains("updated_at")) { issue->setUpdatedAt(jo["updated_at"].toString()); }
+//        if (jo.contains("closed_at"))  { issue->setClosedAt(jo["closed_at"].toString()); }
+
+        if (jo.contains("time_stats")){
+            QJsonObject jtime = jo["time_stats"].toObject();
+            if (jtime.contains("time_estimate")){
+                issue->setTimeEst(jtime["time_estimate"].toInt());
+            }
+            if (jtime.contains("time_spend")){
+                issue->setTimeSpend(jtime["time_spend"].toInt());
+            }
+        }
+        if (jo.contains("weight"))       { issue->setWeight(jo["weight"].toInt()); }
 
         il.append(issue);
     }

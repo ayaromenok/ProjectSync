034c39e784579073aef00cac3add0653d96d0c99
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-11-27 14:22:15 +0100

    #8 add set/get for all params

--- src/main/yuser.h
+++ src/main/yuser.h
@@ -13,6 +13,16 @@ public:
     void dumpToConsole();
     int getId(){return _id;}
     void setId(int id){_id = id;}
+    QString getName(){return _name;}
+    void setName(QString name){_name = name;}
+    QString getUserName(){return _username;}
+    void setUserName(QString username){_username = username;}
+    int getState(){return _state;}
+    void setState(int state){_state = state;}
+    QString getAvatarUrl(){return _avatarUrl;}
+    void setAvatarUrl(QString urlStr){_avatarUrl = urlStr;}
+    QString getWebUrl(){return _webUrl;}
+    void setWebUrl(QString urlStr){_webUrl = urlStr;}
 
 signals:
 

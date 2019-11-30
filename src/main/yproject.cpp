261aff9de545cba8666552876f9bd80f8e5cbe58
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-11-30 12:55:52 +0100

    #12 added load of test users, issues, milestones and notes to project

--- src/main/yproject.cpp
+++ src/main/yproject.cpp
@@ -6,6 +6,8 @@
 #include "../src/main/yissue.h"
 #include "../src/main/ynotes.h"
 
+#include "../src/test/ytestdata.h"
+
 YProject::YProject(QObject *parent) : QObject(parent),
     _isTest(true)
 {
@@ -26,8 +28,14 @@ void
 YProject::useTestValues()
 {
 
-    _ul = new QList<YUser*>;
+    _ul = new QList<YUser*>;    
     _ml = new QList<YMilestone*>;
     _il = new QList<YIssue*>;
     _nl = new QList<YNotes*>;
+    YTestData::loadUsers(_ul);
+    YTestData::loadMilestones(_ml);
+    YTestData::loadIssues(_il);
+    YTestData::loadNotes(_nl);
+    qDebug() << "Loaded: " << _ul->length() << "users, " << _ml->length() << "milestones,"
+             << _il->length() << "issues, " << _nl->length() << "notes";
 }
